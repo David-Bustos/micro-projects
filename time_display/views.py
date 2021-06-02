869b0fb37aab5e70c4285699d4d24371d0b67e78
David Andrés Bustos Oñate <david.bustosonate@gmail.com>
2021-06-02 01:01:20 -0400

    hourglass

---
+++ time_display/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from time import gmtime, strftime, localtime
+    
+def current_td(request):
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", localtime())
+    }
+    return render(request,'index.html', context)
